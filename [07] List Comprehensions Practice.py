a065f3cde3a069674eab54fff5fa2ad6956322f2
AndreyGalaxy <andreybarankevich2.0@gmail.com>
2025-09-04 15:38:47 -0400

    Update 1

--- [07] List Comprehensions Practice.py
+++ [07] List Comprehensions Practice.py
@@ -1,2 +1,5 @@
-a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-print([even for even in a if even % 2 == 0])
+import random
+a = sorted([random.randint(1, 20) for _ in range(random.randint(8, 20))])
+print(f"Randomly Generated List: {a}")
+print(f"Even Numbers: {[even for even in a if even % 2 == 0]}")
+print(f"Odd Numbers: {[odd for odd in a if odd % 2 != 0]}")
